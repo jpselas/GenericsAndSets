37d194aca8ffc7070750c34b90f0ef2814d45648
John <John@10.30.20.12>
2015-11-02 17:31:53 -0600

    more fixes

--- src/ListandSetLab/Challenge4.java
+++ src/ListandSetLab/Challenge4.java
@@ -18,12 +18,18 @@ public class Challenge4 {
         Employee person2 = new Employee("Smith", "Bob","858-56-8885");
         Employee person3 = new Employee("Allen", "Joe","756-75-3564");
         Employee person4 = new Employee("Selas", "John","103-45-5345");
+        Employee person5 = new Employee("Selas", "John","103-45-5345");
+        Employee person6 = new Employee("Selas", "John","103-45-5345");
+        Employee person7 = new Employee("Selas", "John","103-45-5345");
         Set <Employee> employees = new HashSet<Employee>();
         
         employees.add(person1);
         employees.add(person2);
         employees.add(person3);
         employees.add(person4);
+        employees.add(person5);
+        employees.add(person6);
+        employees.add(person7);
         
         for(Employee e : employees){
             System.out.println(e);
